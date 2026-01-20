87a967b0e5cb12a8ec34e427502b2cfdf189b7b0
Mihajlo <mihajlo0903@gmail.com>
2026-01-20 19:00:03 +0100

    [ADD] no unused vars rule implementation

--- src/bin/rusty_judge.rs
+++ src/bin/rusty_judge.rs
@@ -1,4 +1,4 @@
-use rusty_judge::{parse_cli, parse_js_file, RuleContext, NoVar, Rule};
+use rusty_judge::{parse_cli, parse_js_file, RuleContext, NoVar, Rule, NoUnusedVars};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli_commands = parse_cli()?;
@@ -7,8 +7,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .for_each(|command| {
             let (ast, source) = parse_js_file(&command.file_path).expect("Failed to parse file");
             let context = RuleContext::new(ast, source);
-            let no_var = NoVar;
-            println!("{}" ,no_var.check(&context));
+            println!("{:#?}", &context.root);
+            println!("No var: {}" ,NoVar.check(&context));
+            println!("No unused vars: {}", NoUnusedVars.check(&context));
         }
         );
 
--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -1,5 +1,7 @@
 mod rule;
 mod no_var;
+mod no_unused_vars;
 
 pub use rule::*;
+pub use no_unused_vars::*;
 pub use no_var::*;
