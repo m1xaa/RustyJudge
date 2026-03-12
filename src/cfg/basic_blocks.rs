4bfac7d480d845dfa3197bc50b7881397639363e
Mihajlo <mihajlo0903@gmail.com>
2026-03-12 15:53:30 +0100

    [ADD] cfg basic blocks

---
+++ src/cfg/basic_blocks.rs
@@ -0,0 +1,80 @@
+pub type BlockId = usize;
+pub type StatementId = usize;
+pub type SymbolId = usize;
+
+#[derive(Debug, Clone)]
+pub struct Cfg {
+    pub entry: BlockId,
+    pub exit: BlockId,
+    pub blocks: Vec<BasicBlock>,
+}
+
+#[derive(Debug, Clone)]
+pub struct BasicBlock {
+    pub id: BlockId,
+    pub stmts: Vec<StatementInfo>,
+    pub terminator: Terminator,
+    pub predecessors: Vec<BlockId>,
+}
+
+#[derive(Debug, Clone)]
+pub struct StatementInfo {
+    pub id: StatementId,
+    pub kind: StatementKind,
+    pub span: Span,
+    pub defines: Vec<SymbolId>,
+    pub uses: Vec<SymbolId>,
+}
+
+#[derive(Debug, Clone)]
+pub enum StatementKind {
+    VarDecl,
+    Assign,
+    Expr,
+    Return,
+    Break,
+    Continue,
+    Empty,
+}
+
+#[derive(Debug, Clone)]
+pub enum Terminator {
+    Goto(BlockId),
+    Branch {
+        cond_uses: Vec<SymbolId>,
+        then_bb: BlockId,
+        else_bb: BlockId,
+    },
+    Return {
+        value_uses: Vec<SymbolId>,
+    },
+    Unreachable,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+pub struct Span {
+    pub start: u32,
+    pub end: u32,
+}
+
+#[derive(Debug, Clone)]
+pub struct Symbol {
+    pub id: SymbolId,
+    pub name: String,
+    pub decl_span: Span,
+    pub kind: SymbolKind,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum SymbolKind {
+    Var,
+    Let,
+    Const,
+    Param,
+    Function,
+}
+
+#[derive(Debug, Default)]
+pub struct SymbolTable {
+    pub symbols: Vec<Symbol>,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod diagnostics;
 mod linter;
 mod run_cli;
 mod lsp;
+mod cfg;
 
 pub use run_cli::run_cli;
 pub use lsp::run_lsp_server;
