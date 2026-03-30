42ccb6052123151a06bfbc828eb6a49aae1fc838
Mihajlo Orlovic <mihajlo0903@gmail.com>
2026-03-30 14:43:12 +0200

    [ADD] block level liveness analysis

--- src/bin/cfg_test.rs
+++ src/bin/cfg_test.rs
@@ -1,11 +1,13 @@
 use std::path::Path;
-use rusty_judge::{build_cfg_from_root, parse_source, read_js_file};
+use rusty_judge::{build_cfg_from_root, compute_liveness, parse_source, print_liveness, read_js_file};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let source = read_js_file(Path::new("test/cfg.js"))?;
     let root = parse_source(&source)?;
-    println!("{:#?}", &root);
-    let cfg = build_cfg_from_root(root);
+    //println!("{:#?}", &root);
+    let cfg = build_cfg_from_root(root)?;
     println!("{:#?}", &cfg);
+    let liveness = compute_liveness(&cfg);
+    print_liveness(&cfg, &liveness);
     Ok(())
 }
--- src/cfg/mod.rs
+++ src/cfg/mod.rs
@@ -2,5 +2,7 @@ mod basic_blocks;
 mod builder;
 mod resolver;
 mod lowering;
+mod liveness;
 
 pub use lowering::build_cfg_from_root;
+pub use liveness::{compute_liveness, print_liveness};
